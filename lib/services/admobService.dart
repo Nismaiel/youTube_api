a2c0728c9e16ca602f759a6eb0ca20c33ad32639
Nader ismaiel <44040428+Nismaiel@users.noreply.github.com>
2020-07-25 16:48:15 +0200

    Update admobService.dart

--- lib/services/admobService.dart
+++ lib/services/admobService.dart
@@ -4,9 +4,9 @@ class AdMobService{
 
   String getAdMobId(){
     if(Platform.isIOS){
-      return 'ca-app-pub-1913754781691997~8480424041';
+      return 'Your-Ios-ID';
     }else if(Platform.isAndroid){
-      return 'ca-app-pub-1913754781691997~4439013351';
+      return 'Your-Android-Id';
     }
 //return null;
   }
